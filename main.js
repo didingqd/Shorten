400b8abb0ea2ce198b49893c4bd0f5a5a5b6fc68
didingqd <wy_bjj@126.com>
2024-11-08 20:36:30 +0800

    Update main.js

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ function shorturl() {
     .then(function(myJson) {
       res = myJson;
       document.getElementById("addBtn").disabled = false;
-      document.getElementById("addBtn").innerHTML = 'Shorten it';
+      document.getElementById("addBtn").innerHTML = '执行';
 
       // 成功生成短链
       if (res.status == "200") {
